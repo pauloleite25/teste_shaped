c059cdd595b7384cb2ff5a2d05b1cd127f437667
paulo.leite <paulo.leite@fpf.br>
2023-10-20 20:29:44 -0400

    retirando codigo sem uso

--- news/managers.py
+++ news/managers.py
@@ -9,11 +9,3 @@ class LinkManager(models.Manager):
         hash_link = generate_link_hask()
         expiration = timezone.now() + timezone.timedelta(hours=1)
         return self.update_or_create(news=news, hash_link=hash_link, expiration=expiration)
-
-    def is_valid(self):
-        return self.expiration > timezone.now()
-
-    def get_news_data(self):
-        if self.is_valid():
-            return self.news.serialize_data()
-        return None
